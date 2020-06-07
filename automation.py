37c2a66221702723f8189765b3024603d4263551
Anisa <anisahamohamed@gmail.com>
2020-06-07 09:44:28 -0400

    add first selnium automation

---
+++ automation.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+# Create Chrome Driver
+driver = webdriver.Chrome()
+# Get request to Youtube
+driver.get('https://youtube.com')
+# Find searchInput Box
+searchBox = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input')
+#
+searchBox.send_keys('BeyoncéVEVO')
+searchButton = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/button')
+searchButton.click()
+filterButton = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/div/ytd-toggle-button-renderer/a/paper-button')
+filterButton.click()
+channelFilter = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/iron-collapse/div/ytd-search-filter-group-renderer[2]/ytd-search-filter-renderer[2]/a/div/yt-formatted-string')
+channelFilter.click()
+
+myChannel = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-channel-renderer[2]/div/div[2]/a/div[1]/ytd-channel-name/div/div/yt-formatted-string')
+myChannel.click()
